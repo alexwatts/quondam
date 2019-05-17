258cbf3b58d0195cd14566b9076eab644a2464ef
alexwsolidstate <alexw@solidstategroup.com>
2019-05-17 23:55:02 +0100

    Integrated Docker

--- src/test/java/com/ajw/bond/controller/KeyControllerIntegrationTest.java
+++ src/test/java/com/ajw/bond/controller/KeyControllerIntegrationTest.java
@@ -17,7 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.BDDMockito.given;
